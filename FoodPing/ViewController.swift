48e578adad5ceaaeffaa7fa7dffff8fe996675b2
Michael Hollman <hollyfrass@gmail.com>
2014-08-07 09:21:17 -0500

    Initial Commit

---
+++ FoodPing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodPing
+//
+//  Created by Michael Hollman on 8/7/14.
+//  Copyright (c) 2014 Michael Hollman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
